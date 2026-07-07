7b21f50d9e9f708434c62ee190003de557debc80
Jaisal-Gautam <jaisalwork13@gmail.com>
2026-07-07 17:31:32 +0530

    chore: initialize React extension with Vite and Tailwind

---
+++ Frontend-extension/vite.config.js
@@ -0,0 +1,35 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        popup: path.resolve(__dirname, "popup.html"),
+        blocked: path.resolve(__dirname, "blocked.html"),
+        background: path.resolve(__dirname, "src/background/background.js"),
+      },
+
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "background") {
+            return "background.js";
+          }
+
+          return "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
+});
