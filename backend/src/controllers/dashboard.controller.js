dfd9858f699765019518989abf0dfb09a09527bc
Jaisal-Gautam <jaisalwork13@gmail.com>
2026-07-29 19:01:28 +0530

    minor change

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -8,7 +8,6 @@ import {
   getHistory,
   getLifetimeAnalytics,
 } from "../services/analytics.service.js";
-
 export const getDashboard = asyncHandler(async (req, res) => {
   const start = performance.now();
 
@@ -50,39 +49,3 @@ export const getDashboard = asyncHandler(async (req, res) => {
     },
   });
 });
-
-  const [
-    preferences,
-    overview,
-    lifetime,
-    websiteAnalytics,
-    focusAnalytics,
-    history,
-  ] = await Promise.all([
-    getPreferences(req.user._id),
-    getOverview(req.user),
-    getLifetimeAnalytics(req.user),
-    getWebsiteAnalytics(req.user),
-    getFocusAnalytics(req.user),
-    getHistory(req.user, req.query.range ?? "daily"),
-  ]);
-
-
-  res.status(200).json({
-    success: true,
-    message: "Dashboard data fetched successfully.",
-    dashboard: {
-      preferences,
-      analytics: {
-        overview,
-        lifetime,
-        focus: focusAnalytics,
-
-        websites: {
-          usage: websiteAnalytics.websiteAnalytics,
-          blocked: websiteAnalytics.blockedWebsiteAnalytics,
-        },
-        history,
-      },
-    },
-  });
