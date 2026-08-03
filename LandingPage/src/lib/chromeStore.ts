d68c86a28a3585b68d4f4f32c44cc83fa24d6c71
Jaisal-Gautam <jaisalwork13@gmail.com>
2026-08-03 17:13:21 +0530

    Added Store URL

--- LandingPage/src/lib/chromeStore.ts
+++ LandingPage/src/lib/chromeStore.ts
@@ -1,4 +1,4 @@
-export const CHROME_STORE_URL = 'https://chrome.google.com/webstore';
+export const CHROME_STORE_URL = 'https://chromewebstore.google.com/detail/zenmind/eaoccliembamohhchmmigmpaocanihdg?hl=en-GB&authuser=0';
 export const KOFI_URL= 'https://ko-fi.com/jaisalgautam';
 export const installZenMind = () => {
   window.open(CHROME_STORE_URL, '_blank', 'noopener,noreferrer');
